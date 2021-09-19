e2e7c341f7b8f5f9f0f639491d22a29f831310c2
Minquea <arrmo457@student.otago.ac.nz>
2021-09-20 11:36:14 +1200

    2 versions of server to play with

--- serverJ.php
+++ serverJ.php
@@ -1,42 +1,42 @@
 <?php
 session_start();
 
-$fullName = "";
+$username = "";
 $email    = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect("localhost", "root", "", "AnC_ConsentDB");
+$db = mysqli_connect("localhost", "root", "", "login-system");
 
 // register user
-if (isset($_POST['reg_Consultant'])) {
+if (isset($_POST['reg_user'])) {
   // receive all input values from the form
-  $fullName = mysqli_real_escape_string($db, $_POST['fullName']);
+  $username = mysqli_real_escape_string($db, $_POST['username']);
   $email = mysqli_real_escape_string($db, $_POST['email']);
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
-  if (empty($fullName)) { array_push($errors, "Full Name is required"); }
-  elseif (empty($email)) { array_push($errors, "Email is required"); }
-  elseif (empty($password_1)) { array_push($errors, "Password is required"); }
-  elseif ($password_1 != $password_2) {
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
 	array_push($errors, "The two passwords do not match");
   }
 
   // first check the database to make sure 
   // a user does not already exist with the same username and/or email
-  $Consultant_check_query = "SELECT * FROM Consultant WHERE fullName='$fullName' OR email='$email' LIMIT 1";
-  $result = mysqli_query($db, $Consultant_check_query);
-  $Consultant = mysqli_fetch_assoc($result);
+  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
   
-  if ($Consultant) { // if consultant/user exists
-    if ($Consultant['fullName'] === $fullName) {
-      array_push($errors, "Consultant already exists");
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
     }
 
-    if ($Consultant['email'] === $email) {
+    if ($user['email'] === $email) {
       array_push($errors, "email already exists");
     }
   }
@@ -45,10 +45,10 @@ if (isset($_POST['reg_Consultant'])) {
   if (count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
 
-  	$query = "INSERT INTO Consultant (fullName, email, password) 
-  			  VALUES('$fullName', '$email', '$password')";
+  	$query = "INSERT INTO users (username, email, password) 
+  			  VALUES('$username', '$email', '$password')";
   	mysqli_query($db, $query);
-  	$_SESSION['fullName'] = $fullName;
+  	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are now logged in";
   	header('location: index.php');
   }
@@ -56,11 +56,11 @@ if (isset($_POST['reg_Consultant'])) {
 
 // LOGIN USER
 if (isset($_POST['login_user'])) {
-    $fullName = mysqli_real_escape_string($db, $_POST['fullName']);
+    $username = mysqli_real_escape_string($db, $_POST['username']);
     $password = mysqli_real_escape_string($db, $_POST['password']);
   
-    if (empty($fullName)) {
-        array_push($errors, "Full Name is required");
+    if (empty($username)) {
+        array_push($errors, "Username is required");
     }
     if (empty($password)) {
         array_push($errors, "Password is required");
@@ -68,14 +68,14 @@ if (isset($_POST['login_user'])) {
   
     if (count($errors) == 0) {
         $password = md5($password);
-        $query = "SELECT * FROM Consultant WHERE fullName='$fullName' AND password='$password'";
+        $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
         $results = mysqli_query($db, $query);
         if (mysqli_num_rows($results) == 1) {
-          $_SESSION['fullName'] = $fullName;
+          $_SESSION['username'] = $username;
           $_SESSION['success'] = "You are now logged in";
           header('location: index.php');
         }else {
-            array_push($errors, "Wrong Full Name/password combination");
+            array_push($errors, "Wrong username/password combination");
         }
     }
   }
