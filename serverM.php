96f332d9cd97eb6600d80de4ae034478b26dc4ef
samAheenan <sam.heenan@gmail.com>
2021-09-22 14:54:35 +1200

    Update database names to allow access on localhost

--- serverM.php
+++ serverM.php
@@ -6,7 +6,7 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect("localhost", "root", "", "AnC_Sep22");
+$db = mysqli_connect("localhost", "root", "", "rmapro");
 
 // register user
 if (isset($_POST['reg_user'])) {
