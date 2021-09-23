ab96fa419d8129183071fed53b10cddfd4408414
samAheenan <sam.heenan@gmail.com>
2021-09-23 22:34:42 +1200

    Edited view.php to match new index.php and incorporate client feedback.

--- view.php
+++ view.php
@@ -1,36 +1,36 @@
 <?php include('header.php'); ?>
 
-<h1>View Records</h1>
+<!-- <h1>View / Edit Record</h1> -->
 <div class="wrapper">
     <div class="input-section">
         <h3>Consent Details</h3>
 
         <form action="index.php" method="POST" class="form-group">
             <div class="record-input-group">
-                <label for="consent-number">Consent number*</label>
-                <input type="text" name="consent-number" id="consent-number" required>
+                <label for="consentNumber">Consent Number*</label>
+                <input type="text" name="consentNumber" id="consentNumber" required>
             </div>
             <div class="record-input-group">
-                <label for="consent-document">Attach consent issue document</label>
-                <input type="file" id="consent-document" name="consent-document">
+                <label for="consentDoc">Attach Consent Issue Document</label>
+                <input type="file" id="consentDoc" name="consentDoc">
             </div>
             <div class="record-input-group">
-                <label for="consent-issue-date">Issue date*</label>
-                <input type="date" id="consent-issue-date" name="consent-issue-date" required>
-            </div>
-
-            <div class="record-input-group">
-                <label for="consent-lapse-date">Lapse date*</label>
-                <input type="date" id="consent-lapse-date" name="consent-lapse-date" required>
+                <label for="issueDate">Issue Date*</label>
+                <input type="date" id="issueDate" name="issueDate" required>
             </div>
 
             <div class="record-input-group">
-                <label for="keyword">Keywords*</label>
-                <input type="text" name="keyword" id="keyword" required>
+                <label for="lapseDate">Lapse Date*</label>
+                <input type="date" id="lapseDate" name="lapseDate" required>
             </div>
 
             <div class="record-input-group">
-                <input type="submit" value="Save consent">
+                <label for="keywords">Keywords*</label>
+                <input type="text" name="keywords" id="keywords" required>
+            </div>
+
+            <div class="record-input-group">
+                <input type="submit" value="Update Consent">
             </div>
         </form>
     </div>
@@ -40,32 +40,24 @@
 
         <form action="" method="POST" class="form-group">
             <div class="record-input-group">
-                <label for="client-id">Client ID*</label>
-                <input type="text" name="client-id" id="client-id" required>
+                <label for="clientId">Client ID*</label>
+                <input type="text" name="clientId" id="clientId" required>
             </div>
             <div class="record-input-group">
-                <label for="company-name">Company name*</label>
-                <input type="text" name="company-name" id="company-name" required>
+                <label for="company">Company/ Client Name*</label>
+                <input type="text" name="company" id="company" required>
             </div>
             <div class="record-input-group">
-                <label for="client-first-name">Client first name*</label>
-                <input type="text" name="client-first-name" id="client-first-name" required>
+                <label for="clientPhone">Client Phone*</label>
+                <input type="tel" name="clientPhone" id="clientPhone" required>
             </div>
             <div class="record-input-group">
-                <label for="client-surname">Client surname*</label>
-                <input type="text" name="client-surname" id="client-surname" required>
-            </div>
-            <div class="record-input-group">
-                <label for="client-phone">Client phone*</label>
-                <input type="tel" name="client-phone" id="client-phone" required>
-            </div>
-            <div class="record-input-group">
-                <label for="client-email">Client email*</label>
-                <input type="email" name="client-email" id="client-email" required>
+                <label for="clientEmail">Client Email*</label>
+                <input type="email" name="clientEmail" id="clientEmail" required>
             </div>
 
             <div class="record-input-group">
-                <input type="submit" value="Save client">
+                <input type="submit" value="Update Client">
             </div>
         </form>
     </div>
@@ -75,28 +67,71 @@
 
         <form action="" method="POST" class="form-group">
             <div class="record-input-group">
-                <label for="job-number">Job number*</label>
-                <input type="text" name="job-number" id="job-number" required>
+                <label for="conditionNumber">Job Number*</label>
+                <input type="text" name="conditionNumber" id="conditionNumber" required>
             </div>
             <div class="record-input-group">
-                <label for="job-status">Job status*</label>
+                <label for="conditionStatus">Condition Status*</label>
                 <select>
                     <option value=”active”>Active</option>
                     <option value=”inactive”>Inactive</option>
                 </select>
             </div>
             <div class="record-input-group">
-                <label for="consultant-name">Consultant*</label>
+                <label for="consultant">Consultant*</label>
                 <select>
                     <option value=”conrad-anderson”>Conrad Anderson</option>
                     <option value=”don-anderson”>Don Anderson</option>
                 </select>
             </div>
 
+            <button class="add_more_button">Add New Condition</button>
+
+            <div class="add-new-condition"></div>
+
+            <script>
+                $(document).ready(function() {
+                    $('.add_more_button').click(function(e) {
+                        e.preventDefault();
+                        $('.add-new-condition').append('<fieldset class="new-input-part">' +
+                            '<legend>Consent Condition</legend>' +
+                            '<a href="#" class="remove-field">Remove</a><br>' +
+                            '<label for="job-condition">Condition Number*</label><br>' +
+                            '<input type="text" name="job-condition" id="job-condition" placeholder="Enter Condition Number Here" required>' +
+                            '<label for="job-condition">Condition Summary / Details</label><br>' +
+                            '<input type="text" name="job-details" id="job-details" placeholder="Enter Condition Details Here" required>' +
+                            '<label for="condition-date">Condition date*</label><br>' +
+                            '<select><option value=”two-weeks”>2 Weeks</option>' +
+                            '<option value=”four-weeks”>4 Weeks</option><br>' +
+                            '<option value=”one-month”>1 Months</option><br>' +
+                            '<option value=”six-months”>6 Months</option><br>' +
+                            '<option value=”six-months”>Custom</option></select><br>' +
+                            '<label for="condition-reminder-date">Reminder date*</label>' +
+                            '<input type="date" id="condition-reminder-date" name="condition-reminder-date" required>' +
+                            '<label for="condition-status">Condition status*</label>' +
+                            '<select><option value=”active”>Active</option>' +
+                            '<option value=”inactive”>Inactive</option>/select>' +
+                            '</fieldset>');
+
+                        $('.new-input-part').on("click", ".remove-field", function(e) {
+                            e.preventDefault();
+                            $(this).parent('fieldset').remove();
+                        });
+
+                    });
+
+                });
+            </script>
+
             <div class="record-input-group">
-                <label for="job-email">Job email*</label>
-                <input type="email" name="job-email" id="job-email" required>
+                <input type="submit" value="Update Job">
             </div>
+
         </form>
     </div>
+
 </div>
+
+</body>
+
+</html>
