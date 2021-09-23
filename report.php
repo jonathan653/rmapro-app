134b0f052dff70b665323b2a4bda1070f8994984
jchua <jonathan.chua@postgrad.otago.ac.nz>
2021-09-23 19:43:22 +1200

    Edit style.css

--- report.php
+++ report.php
@@ -3,11 +3,18 @@
 <h1>Select your report view</h1>
 
 <form class="report-form-group">
-    <input type="radio" name="report-type" value="all"> View all jobs report<br>
-    <input type="radio" name="report-type" value="one-month"> One-month report<br>
-    <input type="radio" name="report-type" value="three-month"> Three-month report<br>
-    <input type="radio" name="report-type" value="custom"> Custom report<br>
 
-        <button type="submit">Submit</button>
+    <label for="consultant-name">Consultant name*</label>
+    <select>
+        <option value=”conrad”>Conrad Anderson</option>
+        <option value=”don”>Don Anderson</option>
+    </select><br>
+    <br>
+    <input type="radio" name="report-type" value="all" required> View all jobs report<br>
+    <input type="radio" name="report-type" value="one-month" required> One-month report<br>
+    <input type="radio" name="report-type" value="three-month" required> Three-month report<br>
+    <input type="radio" name="report-type" value="custom" required> Custom report<br>
+
+    <button type="submit">Submit</button>
 
 </form>
