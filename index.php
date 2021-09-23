c741384d9a7d251755179bea115db0a0e679bdf9
samAheenan <sam.heenan@gmail.com>
2021-09-23 21:41:47 +1200

    Incorporated client feedback into New Record page.

--- index.php
+++ index.php
@@ -14,20 +14,20 @@
 
         <form action="index.php" method="POST" class="form-group">
             <div class="record-input-group">
-                <label for="consent-number">Consent number*</label>
+                <label for="consent-number">Consent Number*</label>
                 <input type="text" name="consent-number" id="consent-number" required>
             </div>
             <div class="record-input-group">
-                <label for="consent-document">Attach consent issue document</label>
+                <label for="consent-document">Attach Consent Issue Document</label>
                 <input type="file" id="consent-document" name="consent-document">
             </div>
             <div class="record-input-group">
-                <label for="consent-issue-date">Issue date*</label>
+                <label for="consent-issue-date">Issue Date*</label>
                 <input type="date" id="consent-issue-date" name="consent-issue-date" required>
             </div>
 
             <div class="record-input-group">
-                <label for="consent-lapse-date">Lapse date*</label>
+                <label for="consent-lapse-date">Lapse Date*</label>
                 <input type="date" id="consent-lapse-date" name="consent-lapse-date" required>
             </div>
 
@@ -51,23 +51,23 @@
                 <input type="text" name="client-id" id="client-id" required>
             </div>
             <div class="record-input-group">
-                <label for="company-name">Company name*</label>
+                <label for="company-name">Company Name*</label>
                 <input type="text" name="company-name" id="company-name" required>
             </div>
             <div class="record-input-group">
-                <label for="client-first-name">Client first name*</label>
+                <label for="client-first-name">Client First Name*</label>
                 <input type="text" name="client-first-name" id="client-first-name" required>
             </div>
             <div class="record-input-group">
-                <label for="client-surname">Client surname*</label>
+                <label for="client-surname">Client Surname*</label>
                 <input type="text" name="client-surname" id="client-surname" required>
             </div>
             <div class="record-input-group">
-                <label for="client-phone">Client phone*</label>
+                <label for="client-phone">Client Phone*</label>
                 <input type="tel" name="client-phone" id="client-phone" required>
             </div>
             <div class="record-input-group">
-                <label for="client-email">Client email*</label>
+                <label for="client-email">Client Email*</label>
                 <input type="email" name="client-email" id="client-email" required>
             </div>
 
@@ -82,11 +82,11 @@
 
         <form action="" method="POST" class="form-group">
             <div class="record-input-group">
-                <label for="job-number">Job number*</label>
+                <label for="job-number">Job Number*</label>
                 <input type="text" name="job-number" id="job-number" required>
             </div>
             <div class="record-input-group">
-                <label for="job-status">Job status*</label>
+                <label for="job-status">Job Status*</label>
                 <select>
                     <option value=”active”>Active</option>
                     <option value=”inactive”>Inactive</option>
@@ -100,7 +100,7 @@
                 </select>
             </div>
 
-            <button class="add_more_button">Add new condition</button>
+            <button class="add_more_button">Add New Condition</button>
 
             <div class="add-new-condition"></div>
 
@@ -109,13 +109,18 @@
                     $('.add_more_button').click(function(e){
                         e.preventDefault();
                         $('.add-new-condition').append('<fieldset class="new-input-part">' +
-                            '<legend>Job condition</legend>' +
+                            '<legend>Consent Condition</legend>' +
                             '<a href="#" class="remove-field">Remove</a><br>' +
-                            '<input type="text" name="job-condition" id="job-condition" placeholder="Condition number" required>' +
-                            '<input type="text" name="job-details" id="job-details" placeholder="Condition summary/details" required>' +
+                            '<label for="job-condition">Condition Number*</label><br>' +
+                            '<input type="text" name="job-condition" id="job-condition" placeholder="Enter Condition Number Here" required>' +
+                            '<label for="job-condition">Condition Summary / Details</label><br>' +
+                            '<input type="text" name="job-details" id="job-details" placeholder="Enter Condition Details Here" required>' +
                             '<label for="condition-date">Condition date*</label><br>' +
-                            '<select><option value=”two-weeks”>2 weeks</option>' +
-                            '<option value=”four-weeks”>4 weeks</option></select><br>' +
+                            '<select><option value=”two-weeks”>2 Weeks</option>' +
+                            '<option value=”four-weeks”>4 Weeks</option><br>' +
+                            '<option value=”one-month”>1 Months</option><br>' +
+                            '<option value=”six-months”>6 Months</option><br>' +
+                            '<option value=”six-months”>Custom</option></select><br>' +                           
                             '<label for="condition-reminder-date">Reminder date*</label>' +
                             '<input type="date" id="condition-reminder-date" name="condition-reminder-date" required>' +
                             '<label for="condition-status">Condition status*</label>' +
@@ -136,7 +141,7 @@
 
 
             <div class="record-input-group">
-                <input type="submit" value="Save job">
+                <input type="submit" value="Save Job">
             </div>
 
         </form>
