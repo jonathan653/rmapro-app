e78c3aa73ece45018f9d5221f4c2a2920095b763
Minquea <arrmo457@student.otago.ac.nz>
2021-09-25 20:37:38 +1200

    fixed import error. If reoccurs delete the autogenerated  commented out code at top and bottom of file that doesn't look like sql

---
+++ AnC_Sep25.sql
@@ -0,0 +1,197 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 25, 2021 at 10:35 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `AnC_Sep25`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `clientId` varchar(7) NOT NULL,
+  `company` varchar(30) NOT NULL,
+  `clientPhone` int(30) NOT NULL,
+  `clientEmail` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `conditions`
+--
+
+CREATE TABLE `conditions` (
+  `conditionNumber` varchar(4) NOT NULL,
+  `consentNumber` varchar(12) NOT NULL,
+  `details` varchar(2000) NOT NULL,
+  `conditionDate` date NOT NULL,
+  `reminderDate` date NOT NULL,
+  `conditionStatus` enum('open','closed') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `consent`
+--
+
+CREATE TABLE `consent` (
+  `consentNumber` varchar(12) NOT NULL,
+  `issueDate` date NOT NULL,
+  `lapseDate` date NOT NULL,
+  `keywords` varchar(200) NOT NULL,
+  `consentDoc` blob NOT NULL,
+  `address` varchar(300) NOT NULL,
+  `clientId` varchar(7) NOT NULL,
+  `jobNumber` varchar(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_details`
+--
+
+CREATE TABLE `job_details` (
+  `jobNumber` varchar(4) NOT NULL,
+  `jobStatus` enum('open','closed') NOT NULL,
+  `consultantName` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `property`
+--
+
+CREATE TABLE `property` (
+  `address` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `consultantName` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `consultantName`, `email`, `password`) VALUES
+(1, 'jonathan', '', 'jonathan.chua@postgrad.otago.ac.nz', '12345'),
+(2, 'test', '', 'test@test.com', '827ccb0eea8a706c4c34a16891f84e7b'),
+(3, 'test2', '', 'test2@test.com', '827ccb0eea8a706c4c34a16891f84e7b');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`clientId`);
+
+--
+-- Indexes for table `conditions`
+--
+ALTER TABLE `conditions`
+  ADD PRIMARY KEY (`conditionNumber`,`consentNumber`),
+  ADD KEY `consentNumber` (`consentNumber`);
+
+--
+-- Indexes for table `consent`
+--
+ALTER TABLE `consent`
+  ADD PRIMARY KEY (`consentNumber`),
+  ADD KEY `address` (`address`),
+  ADD KEY `clientId` (`clientId`,`jobNumber`),
+  ADD KEY `jobNumber` (`jobNumber`);
+
+--
+-- Indexes for table `job_details`
+--
+ALTER TABLE `job_details`
+  ADD PRIMARY KEY (`jobNumber`),
+  ADD KEY `consultantName` (`consultantName`);
+
+--
+-- Indexes for table `property`
+--
+ALTER TABLE `property`
+  ADD PRIMARY KEY (`address`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `consultantName` (`consultantName`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `conditions`
+--
+ALTER TABLE `conditions`
+  ADD CONSTRAINT `FKconsentNumber` FOREIGN KEY (`consentNumber`) REFERENCES `consent` (`consentNumber`);
+
+--
+-- Constraints for table `consent`
+--
+ALTER TABLE `consent`
+  ADD CONSTRAINT `FKaddress` FOREIGN KEY (`address`) REFERENCES `property` (`address`),
+  ADD CONSTRAINT `FKclientId` FOREIGN KEY (`clientId`) REFERENCES `clients` (`clientId`),
+  ADD CONSTRAINT `FKjobNumber` FOREIGN KEY (`jobNumber`) REFERENCES `job_details` (`jobNumber`);
+
+--
+-- Constraints for table `job_details`
+--
+ALTER TABLE `job_details`
+  ADD CONSTRAINT `FK_consultantName` FOREIGN KEY (`consultantName`) REFERENCES `users` (`consultantName`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
