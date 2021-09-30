6dfe06e47280a57789ace19f7271288eec1364ae
samAheenan <sam.heenan@gmail.com>
2021-09-30 21:41:33 +1300

    Updated View / Edit Record page in line with new layout for index.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ $con = MySQLi_connect(
     "localhost",
     "root",
     "",
-    "AnC_Sep22"
+    "AnC_Sep25"
 );
 if (MySQLi_connect_errno()) {
     echo "Failed to connect to MySQL: " . MySQLi_connect_error();
