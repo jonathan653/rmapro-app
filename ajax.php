931743acdeea12c9b08fb220eebadb1ad9c0d5a3
jchua <jonathan.chua@postgrad.otago.ac.nz>
2021-10-03 19:38:55 +1300

    Edit search feature for jobNumber, company and address

--- ajax.php
+++ ajax.php
@@ -1,20 +1,25 @@
 <?php include ('db.php');
 if (isset($_POST['search'])) {
     $Name = $_POST['search'];
-    $Query = "SELECT username FROM users WHERE username LIKE '%$Name%'";
+    $Query = "SELECT * FROM clients INNER JOIN property INNER JOIN job_details 
+    WHERE company LIKE '%$Name%' OR address LIKE '%$Name%'
+                OR jobNumber LIKE '%$Name%'";
     $ExecQuery = MySQLi_query($db, $Query);
     echo '
 <ul>
    ';
     while ($Result = MySQLi_fetch_array($ExecQuery)) {
         ?>
-        <li onclick='fill("<?php echo $Result['username']; ?>")'>
+        <li onclick='fill("")'>
             <a href="view.php">
-                <?php echo $Result['username']; ?>
+                <?php echo "
+                <table><tr><td>".$Result['jobNumber']."</td></tr>
+                <tr><td>".$Result['company']."</td></tr>".
+                    "<tr><td>".$Result['address']."</td></tr></table>"
+                ?>
         </li></a>
         <?php
     }}
 ?>
 </ul>
-
 
