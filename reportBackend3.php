f30c823e5f2df242f11e2501e5537aa4c8b62803
Minquea <arrmo457@student.otago.ac.nz>
2021-10-04 14:56:29 +1300

    filtering by lapse date

--- reportBackend3.php
+++ reportBackend3.php
@@ -22,7 +22,23 @@ $sql = "SELECT consent.jobNumber, consent.address, clients.company, job_details.
 FROM consent INNER JOIN job_details ON consent.jobNumber=job_details.jobNumber 
 INNER JOIN clients ON consent.clientId=clients.clientId
 INNER JOIN conditions ON consent.consentNumber=conditions.consentNumber
-WHERE reminderDate >= CURRENT_DATE AND reminderDate <= CURRENT_DATE +7 ORDER BY conditionDate";
+WHERE reminderDate >= CURRENT_DATE AND reminderDate <= ADDDATE(CURRENT_DATE, INTERVAL 7 DAY)
+OR (lapseDate>= CURRENT_DATE AND lapseDate<= ADDDATE(CURRENT_DATE, INTERVAL 9 MONTH))
+ORDER BY conditionDate";
+
+$sql2 = "SELECT consent.jobNumber, consent.address, clients.company, job_details.consultantName, conditions.conditionNumber, conditions.conditionDate, consent.keywords
+FROM consent INNER JOIN job_details ON consent.jobNumber=job_details.jobNumber 
+INNER JOIN clients ON consent.clientId=clients.clientId
+INNER JOIN conditions ON consent.consentNumber=conditions.consentNumber
+WHERE (reminderDate > ADDDATE(CURRENT_DATE, INTERVAL 7 day) AND reminderDate <= ADDDATE(CURRENT_DATE, INTERVAL 30 DAY)) 
+OR (lapseDate>= ADDDATE(CURRENT_DATE, INTERVAL 9 MONTH) AND lapseDate<= ADDDATE(CURRENT_DATE, INTERVAL 10 MONTH)) ORDER BY conditionDate";
+
+$sql3 = "SELECT consent.jobNumber, consent.address, clients.company, job_details.consultantName, conditions.conditionNumber, conditions.conditionDate, consent.keywords
+FROM consent INNER JOIN job_details ON consent.jobNumber=job_details.jobNumber 
+INNER JOIN clients ON consent.clientId=clients.clientId
+INNER JOIN conditions ON consent.consentNumber=conditions.consentNumber
+WHERE (reminderDate > ADDDATE(CURRENT_DATE, INTERVAL 30 day) AND reminderDate <= ADDDATE(CURRENT_DATE, INTERVAL 90 DAY)) 
+OR (lapseDate> ADDDATE(CURRENT_DATE, INTERVAL 10 MONTH) AND lapseDate<= ADDDATE(CURRENT_DATE, INTERVAL 12 MONTH)) ORDER BY conditionDate";
 
 $str = "<html><body>";
 if($result = mysqli_query($link, $sql)){
@@ -60,6 +76,75 @@ if($result = mysqli_query($link, $sql)){
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
+
+if($result2 = mysqli_query($link, $sql2)){
+    if(mysqli_num_rows($result2) > 0){
+        $str = $str. "<h3>Rest of the coming month</h3>";
+        $str = $str. "<table>";
+            $str = $str.   "<tr>";
+                $str = $str .  "<th>Job Number</th>";
+                $str = $str .  "<th>Keywords</th>";
+                $str = $str .  "<th>Client Name/ Company</th>";
+                $str = $str .  "<th>Address</th>";
+                $str = $str .  "<th>Condition Date</th>";
+                $str = $str .  "<th>Condition Number</th>";
+                $str = $str .  "<th>Consultant Name</th>";
+            $str = $str .  "</tr>";
+        while($row = mysqli_fetch_array($result2)){
+            $str = $str .  "<tr>";
+                $str = $str .  "<td>" . $row['jobNumber'] . "</td>";
+                $str = $str .  "<td>" . $row['keywords'] . "</td>";
+                $str = $str .  "<td>" . $row['company'] . "</td>";
+                $str = $str .  "<td>" . $row['address'] . "</td>";
+                $str = $str .  "<td>" . $row['conditionDate'] . "</td>";
+                $str = $str .  "<td>" . $row['conditionNumber'] . "</td>";
+                $str = $str .  "<td>" . $row['consultantName'] . "</td>";
+            $str = $str .  "</tr>";
+        }
+        $str = $str .  "</table>";
+        // Free result set
+        mysqli_free_result($result2);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql2. " . mysqli_error($link);
+}
+
+if($result3 = mysqli_query($link, $sql3)){
+    if(mysqli_num_rows($result3) > 0){
+        $str = $str. "<h3>Rest of the coming 90 Days</h3>";
+        $str = $str. "<table>";
+            $str = $str.   "<tr>";
+                $str = $str .  "<th>Job Number</th>";
+                $str = $str .  "<th>Keywords</th>";
+                $str = $str .  "<th>Client Name/ Company</th>";
+                $str = $str .  "<th>Address</th>";
+                $str = $str .  "<th>Condition Date</th>";
+                $str = $str .  "<th>Condition Number</th>";
+                $str = $str .  "<th>Consultant Name</th>";
+            $str = $str .  "</tr>";
+        while($row = mysqli_fetch_array($result3)){
+            $str = $str .  "<tr>";
+                $str = $str .  "<td>" . $row['jobNumber'] . "</td>";
+                $str = $str .  "<td>" . $row['keywords'] . "</td>";
+                $str = $str .  "<td>" . $row['company'] . "</td>";
+                $str = $str .  "<td>" . $row['address'] . "</td>";
+                $str = $str .  "<td>" . $row['conditionDate'] . "</td>";
+                $str = $str .  "<td>" . $row['conditionNumber'] . "</td>";
+                $str = $str .  "<td>" . $row['consultantName'] . "</td>";
+            $str = $str .  "</tr>";
+        }
+        $str = $str .  "</table>";
+        // Free result set
+        mysqli_free_result($result3);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql3. " . mysqli_error($link);
+}
+
 $str = $str . "</body></html>";
 // Close connection
 mysqli_close($link);
